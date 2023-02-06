f3bc4b5be2eec1cd59d18ef95377020bcfbc83e9
eivv77 <nurlan4ik2002@gmail.com>
2023-02-06 08:51:57 +0400

    test

--- DiaBot/Connection_String.cs
+++ DiaBot/Connection_String.cs
@@ -8,12 +8,19 @@ namespace DiaBot
 {
     public class Connection_String
     {
-        private string connectionString = @"Data Source=TXBBNUEYVAZOV\WORKEIVV77;Database=dia_bot;Trusted_Connection=True;MultipleActiveResultSets=True";
+        private string connectionString = @"Data Source=TXBBNUEYVAZOV\WORKSTATION;Database=Dia_Bot;Trusted_Connection=True;MultipleActiveResultSets=True";
+        private const string TOKEN = "5774023928:AAH0Zj0hgqpgErS0KX5mOeDb9Ze-A_C_BQw";
 
         public string Connection_string
         {
             get { return connectionString; }
 
         }
+
+        public string Connection_token
+        {
+            get { return TOKEN; }
+        }
+
     }
 }
--- DiaBot/Program.cs
+++ DiaBot/Program.cs
@@ -3,7 +3,7 @@ using System.Data.SqlClient;
 using Telegram.Bot;
 
 Connection_String con = new Connection_String();
-const string TOKEN = "5774023928:AAH0Zj0hgqpgErS0KX5mOeDb9Ze-A_C_BQw";
+//const string TOKEN = "5774023928:AAH0Zj0hgqpgErS0KX5mOeDb9Ze-A_C_BQw";
 
 while (true)
 {
@@ -31,9 +31,26 @@ void RegisterUser(string username)
     }
 }
 
+int CheckUser(string username)
+{
+    using (SqlConnection connection = new SqlConnection(con.Connection_string))
+    {
+
+        connection.Open();
+        SqlCommand comm = new SqlCommand();
+        comm.Connection = connection;
+        comm.CommandText = "SELECT COUNT(*) FROM Users WHERE Username= @username ";
+        comm.Parameters.AddWithValue("@username", username);
+		int count = (int)comm.ExecuteScalar();
+        connection.Close();
+
+		return count;
+    }
+}
+
 async Task GetMessages()
 {
-	TelegramBotClient bot = new TelegramBotClient(TOKEN);
+	TelegramBotClient bot = new TelegramBotClient(con.Connection_token);
 	int offset = 0;
 	int timeout = 0;
 
@@ -48,15 +65,24 @@ async Task GetMessages()
 			{
 				var message = update.Message;
 
-				if (message.Text == "DiaBot")
+				if (message?.Text == "")
 				{
 					Console.WriteLine("Получено сообщение: " + message.Text);
-					await bot.SendTextMessageAsync(message.Chat.Id, "Привет НУРЛАНЧИК, я твой бот(микаил), слшуюась и повинуюсь тебя");
+					await bot.SendTextMessageAsync(message.Chat.Id, "Привет, я твой бот, для регистрации напишите /reg");
 				}
-				if (message.Text == "/reg")
+				if (message?.Text == "/reg")
 				{
-					RegisterUser(message.Chat.Username.ToString());
-					await bot.SendTextMessageAsync(message.Chat.Id, "Пользователь зарегистрирован");
+					var username = message.Chat.Username;
+
+					if (CheckUser(username) >= 1)
+					{
+                        await bot.SendTextMessageAsync(message.Chat.Id, "Пользователь УЖЕ зарегистрирован");
+                    }
+                    else
+					{
+                        RegisterUser(message.Chat.Username.ToString());
+                        await bot.SendTextMessageAsync(message.Chat.Id, "Вы успешно зарегистрировались");
+                    }
                 }
 				offset = update.Id + 1;
 			}
